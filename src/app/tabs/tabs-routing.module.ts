811202f4e92b0f939028fa89b3c4f37486032113
Aldo Bernardes <aldo-maciel@hotmail.com>
2020-05-13 22:03:02 -0300

    feat: remove initial cli structure

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -3,38 +3,39 @@ import { RouterModule, Routes } from '@angular/router';
 import { TabsPage } from './tabs.page';
 
 const routes: Routes = [
-  {
-    path: 'tabs',
-    component: TabsPage,
-    children: [
-      {
-        path: 'tab1',
-        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
-      },
-      {
-        path: 'tab2',
-        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
-      },
-      {
-        path: 'tab3',
-        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
-      },
-      {
+    {
+        path: 'tabs',
+        component: TabsPage,
+        children: [
+            {
+                path: 'home',
+                loadChildren: () => import('../home/home.module').then(m => m.HomePageModule)
+            },
+            {
+                path: 'employees',
+                loadChildren: () => import('../employees/employees.module').then(m => m.EmployeesPageModule)
+            },
+            {
+                path: 'about',
+                loadChildren: () => import('../about/about.module').then(m => m.AboutPageModule)
+            },
+            {
+                path: '',
+                redirectTo: '/tabs/home',
+                pathMatch: 'full'
+            }
+        ]
+    },
+    {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/home',
         pathMatch: 'full'
-      }
-    ]
-  },
-  {
-    path: '',
-    redirectTo: '/tabs/tab1',
-    pathMatch: 'full'
-  }
+    }
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+    imports: [ RouterModule.forChild(routes) ],
+    exports: [ RouterModule ]
 })
-export class TabsPageRoutingModule {}
+export class TabsPageRoutingModule {
+}
